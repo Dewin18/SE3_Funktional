2ce0123b03a68fb33813df56c35f2bcb148b529a
Josua Spisak <gepard@gmx.de>
2017-12-14 23:08:32 +0100

    BugFix
    An sich hatte ich den nächsten stand falsch berechnent weil ich eine veraltete metode benutzt habe

--- 07/GameOfLife.rkt
+++ 07/GameOfLife.rkt
@@ -1,6 +1,6 @@
 #lang racket
 ;;;2 Game Of Life
-(define N 30)
+(define N 5)
 ;;2.1
 ;this function gives us a list of list with basicly the cordinates of the Game so we have 1-30 places in row 1 1-30 places in row 2 and so on we give out the row number and then the seats
 ;using this function gives us the advantage that we can find the neighbours wich will be important for the game and we can set N however we want
@@ -45,18 +45,18 @@
       (rectangle 0 0 "solid" "black") ))
 ;;2.3
 ;analyses the Neigbhours of a cell at the position X Y and gives the state of every Neighbour from it back as a list
-(define (analyseNeighbours X Y)
+(define (analyseNeighbours X Y oldGameState)
   (list
-   (analyseNeighbour (+ X 1) Y)
-   (analyseNeighbour (- X 1) Y)
-   (analyseNeighbour  X  (+ Y 1))
-   (analyseNeighbour  X  (-  Y 1))
-   (analyseNeighbour (+ X 1) (- Y 1))
-   (analyseNeighbour (+ X 1) (+ Y 1))
-   (analyseNeighbour (- X 1) (+ Y 1))
-   (analyseNeighbour (- X 1) (- Y 1))))
+   (analyseNeighbour (+ X 1) Y oldGameState)
+   (analyseNeighbour (- X 1) Y oldGameState)
+   (analyseNeighbour  X  (+ Y 1)oldGameState)
+   (analyseNeighbour  X  (-  Y 1)oldGameState)
+   (analyseNeighbour (+ X 1) (- Y 1)oldGameState)
+   (analyseNeighbour (+ X 1) (+ Y 1)oldGameState)
+   (analyseNeighbour (- X 1) (+ Y 1)oldGameState)
+   (analyseNeighbour (- X 1) (- Y 1)oldGameState)))
 ;analyses the state of a cell at Position X Y
-(define (analyseNeighbour X Y)
+(define (analyseOldNeighbour X Y)
   (let ([Row (cdr (assoc (cons X '()) (gameStart 0 N)))])
     (getXElementFromList (+ 1 (* 2 Y)) Row)))
 ;gives back the specific element at place X from a list
@@ -92,22 +92,22 @@
   n))
 
 ;this functions finds the compllete new game state for one game state according to the rules using the function that does this for one state on all states
-(define (nextGameState X oldGameState)
+(define (nextGameState X oldGameState safeOldGameState)
   (if (< 0  (length oldGameState) )
       (cons 
-   (cons (cons X '()) (nextGameStateRow X (cdr (car oldGameState)))) (nextGameState (+ X 1) (cdr oldGameState)))
+   (cons (cons X '()) (nextGameStateRow X (cdr (car oldGameState)) safeOldGameState)) (nextGameState (+ X 1) (cdr oldGameState) safeOldGameState))
   '()))
 ;this functions find the new game state of one row using the rules and the old row
-(define (nextGameStateRow X oldGameStateRow)
+(define (nextGameStateRow X oldGameStateRow oldGameState)
   (if (< 1 (length oldGameStateRow))
        (flatten (cons   (flatten (cons
                                   (car oldGameStateRow)
-                                  (nextGameStatePlace X    (car oldGameStateRow) (car (cdr oldGameStateRow)))))
-                        (nextGameStateRow  X (cddr oldGameStateRow))))
+                                  (nextGameStatePlace X    (car oldGameStateRow) (car (cdr oldGameStateRow)) oldGameState)))
+                        (nextGameStateRow  X (cddr oldGameStateRow) oldGameState)))
       '()))
 ;this function finds one new state using the rules and the old states
-(define (nextGameStatePlace X Y Current)
-  (let ([NeighbourStates (analyseNeighboursWithEdge X Y)])
+(define (nextGameStatePlace X Y Current oldGameState)
+  (let ([NeighbourStates (analyseNeighboursWithEdge X Y oldGameState)])
   (cond [(= 3 (countLiving NeighbourStates 0))
          "a"]
          [(< 3 (countLiving NeighbourStates 0))
@@ -119,62 +119,62 @@
                  [else "d"])
           ])))
 ;this function is like the old analysNeighbour function only that we also deal with cells wich have less then 8 neihbours
-(define (analyseNeighboursWithEdge X Y)
+(define (analyseNeighboursWithEdge X Y oldGameState)
   (cond [(= 0 X Y) 
          (list
-          (analyseNeighbour (+ X 1) (+ Y 1))
-          (analyseNeighbour X (+ Y 1))
-          (analyseNeighbour (+ X 1) Y))]
-        [(= 30 X Y)
+          (analyseNeighbour (+ X 1) (+ Y 1)oldGameState)
+          (analyseNeighbour X (+ Y 1)oldGameState)
+          (analyseNeighbour (+ X 1) Y oldGameState))]
+        [(= N X Y)
          (list
-          (analyseNeighbour (- X 1) (- Y 1))
-          (analyseNeighbour X (- Y 1))
-          (analyseNeighbour (- X 1) Y))]
-        [(and (= 30 Y) (= 0 X))
+          (analyseNeighbour (- X 1) (- Y 1) oldGameState)
+          (analyseNeighbour X (- Y 1) oldGameState)
+          (analyseNeighbour (- X 1) Y oldGameState))]
+        [(and (= N Y) (= 0 X))
          (list
-          (analyseNeighbour (+ X 1) (- Y 1))
-          (analyseNeighbour X (- Y 1))
-          (analyseNeighbour (+ X 1) Y))]
-        [(and (= 30 X) (= 0 Y))
+          (analyseNeighbour (+ X 1) (- Y 1)oldGameState)
+          (analyseNeighbour X (- Y 1)oldGameState)
+          (analyseNeighbour (+ X 1) Y oldGameState))]
+        [(and (= N X) (= 0 Y))
          (list
-          (analyseNeighbour (+ Y 1) (- X 1))
-          (analyseNeighbour Y (- X 1))
-          (analyseNeighbour (+ Y 1) X))]
+          (analyseNeighbour  (- X 1) (+ Y 1)oldGameState)
+          (analyseNeighbour  (- X 1) Y oldGameState)
+          (analyseNeighbour  X (+ Y 1)  oldGameState))]
         [(= 0 X) 
-         (oneEdge X Y +)]
+         (oneEdge X Y + oldGameState)]
         [(= 0 Y)
-         (oneEdge Y X +)]
-        [ (= 30 X) 
-         (oneEdge X Y -)]
-        [ (= 30 Y) 
-         (oneEdge Y X -)]
-        [else (analyseNeighbours X Y)]))
+         (oneEdge Y X + oldGameState)]
+        [ (= N X) 
+         (oneEdge X Y - oldGameState)]
+        [ (= N Y) 
+         (oneEdge Y X - oldGameState)]
+        [else (analyseNeighbours X Y oldGameState)]))
 ;this function deals with the cases of the analyseNeighboursWithEdges in wich we have cells with 5 neighbours 
-(define (oneEdge Edge NotEdge op )
+(define (oneEdge Edge NotEdge op oldGameState)
          (list
-          (analyseNeighbour (op Edge 1) (+ NotEdge 1))
-          (analyseNeighbour Edge (+ NotEdge 1))
-          (analyseNeighbour (op Edge 1) NotEdge)
-          (analyseNeighbour (op Edge 1) (- NotEdge 1))
-          (analyseNeighbour Edge (- NotEdge 1))))
+          (analyseNeighbour (op Edge 1) (+ NotEdge 1) oldGameState)
+          (analyseNeighbour Edge (+ NotEdge 1)oldGameState)
+          (analyseNeighbour (op Edge 1) NotEdge oldGameState)
+          (analyseNeighbour (op Edge 1) (- NotEdge 1)oldGameState)
+          (analyseNeighbour Edge (- NotEdge 1)oldGameState)))
 ;;2.4
 (require 2htdp/universe)
 ;a start for the animation
 (define gamestart
-  (gameStart 0 30))
+  (gameStart 0 N))
 ;the function that does the animation
 (define (gameOfLife-sim tick)
   (big-bang
    (make-gameOfLife-universe gamestart)
    (on-tick next-frame tick)
-   (to-draw show-gameOfLife-world 300 300)
+   (to-draw show-gameOfLife-world (+ 10 (* N 10)) (+ 10 (* N 10)))
    (name "Game of Life2")))
 ;our universe it takes a state as input
 (define-struct gameOfLife-universe (state))
 ;the function that determines what happens on each tick
 (define (next-frame world)
          (let ([new-world (make-gameOfLife-universe
-                           (nextGameState 0 (gameOfLife-universe-state world)))])
+                           (nextGameState 0 (gameOfLife-universe-state world)(gameOfLife-universe-state world)))])
            new-world))
 ;the fucntion that draws each picture
 (define (show-gameOfLife-world world)
@@ -184,9 +184,12 @@
   (drawCurrentState 0 state))
 ;used by currentState to draw a picture
 (define (drawCurrentState start state)
-  (if (<= start  30 )
-      (above (drawRow (cdr (assoc (cons start '()) state)) 0 30) (drawCurrentState (+ 1 start) state))
+  (if (<= start  N )
+      (above (drawRow (cdr (assoc (cons start '()) state)) 0 N) (drawCurrentState (+ 1 start) state))
       (rectangle 0 0 "solid" "black")))
-
+;analyses the neighbours from the old state
+(define (analyseNeighbour X Y oldGameState)
+  (let ([Row (cdr (assoc (cons X '()) oldGameState))])
+    (getXElementFromList (+ 1 (* 2 Y)) Row)))
 ;the call for the animation (the calculation is reaaaaaaaaaaaaaaaaaaally slow so it takes a while for each picture to show          
 (animate( gameOfLife-sim 1))
